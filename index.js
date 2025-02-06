ea54145d13ddb8c0eec5ccf1d8ad9e25e7c17f16
JackGouldingay <jackgouldigay@outlook.com>
2025-02-06 21:14:36 +0000

    Added a message when a new request is amde

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000
+const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World 3!')
-})
+  console.log(`A new request from ${req.host}`);
+
+  res.send('Hello World 3!');
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
